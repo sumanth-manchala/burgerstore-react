cc4f72430227ac28e92160dc92b4d3437f93a00b
sumanth-manchala <manchala.msr@gmail.com>
2020-04-13 22:31:07 +0530

    Added gaurding routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
@@ -17,19 +17,36 @@ class App extends Component{
   }
 
   render(){
+    let routes = (
+      <Switch>
+          <Route path="/auth" component={Auth} />
+          <Route path="/" exact component={BurgerBuilder} />
+          <Redirect to="/" />
+      </Switch>
+    );
+    if(this.props.isAuth) {
+      routes = (
+        <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
+            <Route path="/logout" component={Logout} />
+            <Route path="/" exact component={BurgerBuilder} />
+            <Redirect to="/" /> 
+        </Switch>
+      );
+    }
     return(
       <div>
           <Layout>
-            <Switch>
-              <Route path="/checkout" component={Checkout} />
-              <Route path="/orders" component={Orders} />
-              <Route path="/auth" component={Auth} />
-              <Route path="/logout" component={Logout} />
-              <Route path="/" exact component={BurgerBuilder} />
-            </Switch>
+            {routes}
           </Layout>
       </div>
     );
+  }
+}
+const mapStateToProps = state => {
+  return {
+    isAuth: state.auth.token != null
   }
 }
 const mapDispatchToProps = dispatch => {
@@ -38,4 +55,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
